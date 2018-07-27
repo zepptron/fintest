0be18af39012110707b7f5baca72734323d2d2b9
zepp <recyc@posteo.de>
2018-07-27 12:59:28 +0200

    init

--- code/main.go
+++ code/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+  "errors"
   "fmt"
+  "github.com/prometheus/client_golang/prometheus"
   "io/ioutil"
   "log"
   "net/http"
@@ -9,19 +11,32 @@ import (
   "time"
 )
 
+var (
+  curls = prometheus.NewCounter(prometheus.CounterOpts{
+    Name:   "webtest_connections_total",
+    Help:   "Number of checks",
+    ConstLabels: prometheus.Labels{"type": "http"},
+  })
+)
+
+func init() {
+  prometheus.MustRegister(curls)
+}
+
 func main() {
     port := portcheck()
-
     fmt.Fprintf(os.Stdout, "Start listening on :%s\n", port)
     hostname, _ := os.Hostname()
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        config := readconf()
-        zeit := time.Now().Format("2006/01/02 15:04:05")
-        fmt.Fprintf(os.Stdout, "%s stdout for logging: %s\n", zeit, config)
+        _, config := readconf()
+        timestamp := time.Now().Format("02/01/2006 15:04:05")
+        fmt.Fprintf(os.Stdout, "%s stdout for logging: %s\n", timestamp, config)
         fmt.Fprintf(w, "Hostname: \n%s\n", hostname)
         fmt.Fprintf(w, "Port: %s\n\n", port)
         fmt.Fprintf(w, "Configfileinput: \n%s", config)
+        curls.Inc()
     })
+    metrics()
     log.Fatal(http.ListenAndServe(":" + port, nil))
 }
 
@@ -34,12 +49,18 @@ func portcheck() (string) {
     return port
 }
 
-func readconf() (string) {
+func readconf() (string, error) {
     // read configfile
     b, err := ioutil.ReadFile("config.file")
     if err != nil {
-        fmt.Print(err)
+        return "", errors.New("file not available.")
+    } else {
+        str := string(b)
+        return str, nil
     }
-    str := string(b)
-    return str
+}
+
+func metrics() {
+    // expose metrics @ /metrics
+    http.Handle("/metrics", prometheus.Handler())
 }
